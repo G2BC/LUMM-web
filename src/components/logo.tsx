32bab6c5c6a886451b9df15f25b81b13f04062b3
Ernesto Junior <ernesto.sjunior@hotmail.com>
2025-06-10 23:34:29 -0300

    :sparkles: style: adjust header height and logo text size for improved visual consistency

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,8 +5,8 @@ export function LummLogo() {
     <div className="flex items-center gap-2">
       <img src={logo} alt="Logo LUMM" />
       <div className="flex flex-col text-white font-(family-name:--font-syne) text-nowrap overflow-hidden">
-        <span className="font-extrabold text-[25px] leading-none">LUMM</span>
-        <span className="text-xs">Luminescent Mushrooms</span>
+        <span className="font-extrabold text-[29.3px] leading-none">LUMM</span>
+        <span className="text-sm">Luminescent Mushrooms</span>
       </div>
     </div>
   );
