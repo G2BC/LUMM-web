d7bb31c6ce7f22b07af19d09f26a72071e44f1d7
Ernesto Junior <ernesto.sjunior@hotmail.com>
2025-09-21 02:42:50 -0300

    feat: new distributtion

--- src/pages/distribution.tsx
+++ src/pages/distribution.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
-import distributionEn from "@/assets/distribution_en.webp";
-import distributionPt from "@/assets/distribution_pt.webp";
+import distributionEn from "@/assets/distribution_en.png";
+import distributionPt from "@/assets/distribution_pt.png";
 import { useState } from "react";
 import { Loader2 } from "lucide-react";
 
@@ -15,7 +15,7 @@ export default function DistributionPage() {
 
   return (
     <section className="container mx-auto pt-10 px-4">
-      <h1 className="font-bold text-[22px] sm:text-[28px]">{t("distribution.title")}</h1>
+      <h1 className="font-bold text-[22px] sm:text-[28px] text-white">{t("distribution.title")}</h1>
       <div className="py-10 overflow-x-auto mb-10 scrollbar-hide">
         <div className="min-w-max md:min-w-0">
           {!loaded && (
