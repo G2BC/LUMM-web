ee70ac65d3f480b232bf271be5e31034c373c50a
Ernesto Junior <ernesto.sjunior@hotmail.com>
2025-09-22 21:24:59 -0300

    fix: include correct id on buttons

--- src/pages/species/index.tsx
+++ src/pages/species/index.tsx
@@ -1,10 +1,13 @@
 import Slide from "@/pages/species/components/slide";
-import { Loader2 } from "lucide-react";
+import { ChevronLeft, Loader2 } from "lucide-react";
 import "@/assets/css/slide.css";
 import { useSpeciesPage } from "./useSpeciesPage";
+import { useNavigate } from "react-router";
+import { Button } from "@/components/ui/button";
 
 export default function SpeciesPage() {
   const { dados, loading } = useSpeciesPage();
+  const navigate = useNavigate();
 
   if (loading) {
     return (
@@ -20,16 +23,26 @@ export default function SpeciesPage() {
     ?.filter(Boolean);
 
   return (
-    <div className="container mx-auto px-4 my-10 text-white grid grid-cols-1 xl:grid-cols-2 gap-10">
-      <div className="text-white max-xl:order-1">
-        <h1 className="text-[34px] xl:text-[50px] font-bold leading-[38px] xl:leading-[54px] mb-8">
-          {dados?.scientific_name}
-        </h1>
-        <h2>Linhagem: {dados?.lineage}</h2>
-        <h3>Família: {dados?.family}</h3>
-      </div>
+    <div className="container mx-auto px-4 my-10 text-white">
+      <Button
+        variant="link"
+        className="mb-6 text-base font-bold !px-0"
+        onClick={() => navigate(-1)}
+      >
+        <ChevronLeft className="w-4 h-4" />
+        Voltar
+      </Button>
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-10">
+        <div className="text-white max-xl:order-1">
+          <h1 className="text-[34px] xl:text-[50px] font-bold leading-[38px] xl:leading-[54px] mb-8 italic">
+            {dados?.scientific_name}
+          </h1>
+          <h2>Linhagem: {dados?.lineage}</h2>
+          <h3>Família: {dados?.family}</h3>
+        </div>
 
-      {!!photos?.length && <Slide slides={photos as string[]} />}
+        {!!photos?.length && <Slide slides={photos as string[]} />}
+      </div>
     </div>
   );
 }
